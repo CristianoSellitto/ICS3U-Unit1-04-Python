494b5941bccfad56f8e3f78705da929a5d3b7d90
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-09-19 15:19:47 +0000

    Added program

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Cristiano Sellitto
+# Created in September 2022
+# A program that finds the area and perimeter or a rectangle
+
+
+def main():
+    # Finds the area and perimeter of a 5cm by 3cm rectangle
+
+    print("A rectangle is 5cm by 3cm")
+    print("\nThis rectangle's area is {}cm²".format(5 * 3))
+    print("This rectangle's perimeter is {}cm".format(2 * (5 + 3)))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
